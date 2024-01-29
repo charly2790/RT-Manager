0525ea47dd70206d3b58199ae8b032f60a68a925
charly <cbarrionuevo@fidelatam.com>
2024-01-29 09:37:56 -0300

    Limpieza de código obsoleto

--- routes/usuarios.routes.js
+++ routes/usuarios.routes.js
@@ -1,11 +1,10 @@
 import { Router } from 'express';
 import multer from 'multer';
-import { create,sayhello } from '../controllers/usuarios.controller.js';
+import { create } from '../controllers/usuarios.controller.js';
 
 const upload = multer()
 const router = Router();
 
 router.post('/usuarios',upload.none(), create);
-router.get('/usuarios',upload.none(), sayhello);
 
 export default router;
