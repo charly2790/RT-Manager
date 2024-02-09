8ee7dac5ec13fdf53851a4b5a15515b1e37206b0
charly <cbarrionuevo@fidelatam.com>
2024-02-09 09:00:12 -0300

    Incorporación de nuevas rutas asociadas a modelos tipoSesion, sesionEntrenamiento y entrenamiento

--- server.js
+++ server.js
@@ -5,9 +5,13 @@ import methodOverride from 'method-override';
 import bodyParser from 'body-parser';
 import usuarioRoutes from './routes/usuarios.routes.js';
 import sesionesRoutes from './routes/sesiones.routes.js';
-import rolesRoutes from './routes/roles.routes.js'
-import permisosRoutes from './routes/permisos.routes.js'
-import permisosRolRoutes from './routes/permisosRol.routes.js'
+import rolesRoutes from './routes/roles.routes.js';
+import permisosRoutes from './routes/permisos.routes.js';
+import permisosRolRoutes from './routes/permisosRol.routes.js';
+import tiposSesionRoutes from './routes/tiposSesion.routes.js';
+import sesionesEntrenamiento from './routes/sesionesEntrenamiento.routes.js';
+import entrenamientos from './routes/entrenamientos.routes.js';
+import { models } from './models/index.js';
 const app = express();
 
 dbsConnection();
@@ -21,6 +25,9 @@ app.use(sesionesRoutes);
 app.use(rolesRoutes);
 app.use(permisosRoutes);
 app.use(permisosRolRoutes);
+app.use(tiposSesionRoutes);
+app.use(sesionesEntrenamiento);
+app.use(entrenamientos);
 
 const responderPeticion = (req, res) => {
     res.send('¡Esta será una gran aplicación!');
