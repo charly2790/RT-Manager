a2df1c6dcf6c86476b9eeb8c7b159befeab6bd69
charly <cbarrionuevo@fidelatam.com>
2024-06-17 07:52:39 -0300

    Se corrige controller create sesiones entrenamiento

--- controllers/sesionesEntrenamiento.controller.js
+++ controllers/sesionesEntrenamiento.controller.js
@@ -1,44 +1,74 @@
 import SesionEntrenamiento from "../models/SesionEntrenamiento.js";
 
-export const create = async (req, res) => {
-    
-    const { idSuscripcion, fechaSesion, Objetivo, Completado, idTipoSesion, idEdicionCarrera, idUserLogged } = req.body;
+const validarSesiones = (sesiones) => {
 
-    if(!idSuscripcion || !fechaSesion || !Objetivo ||  !idTipoSesion){
-        return res.status(400).json('Missing fields');
-    }
+    return sesiones.reduce((result, sesion) => {
 
-    let data = {
-        idSuscripcion,
-        fechaSesion,
-        Objetivo,
-        Completado: false,
-        idTipoSesion,
-        idEdicionCarrera: idEdicionCarrera?idEdicionCarrera:undefined,
-        idUsuarioCreador: idUserLogged,
-        idUsuarioModificador: idUserLogged
-    };
+        const { idSuscripcion, fechaSesion, Objetivo, idTipoSesion } = sesion;
+
+        if (!idSuscripcion || !fechaSesion || !Objetivo || !idTipoSesion) {
+            result.valid = false;
+            result.message.push(`Fecha: ${fechaSesion} - Objetivo: ${Objetivo} | Datos faltantes`);
+        }
+
+        if (existeSesion(sesion)) {
+            result.valid = false;
+            result.message.push(`Fecha: ${fechaSesion} - Objetivo: ${Objetivo} | Ya existe una sesión para esta suscripción`);
+        }
+        
+        return result;
+    }, { valid: true, message: [] })
+
+}
+
+const existeSesion = async ({ idSuscripcion, idTipoSesion, fechaSesion, Objetivo }) => {
+
+    let sesiones = [];
 
     try {
-        let newSesionEntrenamiento = await SesionEntrenamiento.create(data);
-        return res.status(200).json(newSesionEntrenamiento)
+        sesiones = await SesionEntrenamiento.findAll({ where: { idSuscripcion, idTipoSesion, fechaSesion, Objetivo } });
+        return sesiones.length > 0;
+    } catch (error) {
+        console.log(`Error al recuperar sesiones de entrenamiento \n ${error}`);
+        return true;
+    }
+
+}
+
+export const create = async (req, res) => {
+
+    const { sesiones, idUserLogged } = req.body;
+
+    const sesionesValidas = validarSesiones(sesiones);
+
+    if (!sesionesValidas) {
+        return res.status(400).json(sesionesValidas.message);
+    }
+
+    let data = sesiones.map(sesion =>({
+        ...sesion,
+        idUsuarioCreador: idUserLogged,
+        idUsuarioModificador:idUserLogged
+    }))    
+
+    try {
+        let status = await SesionEntrenamiento.bulkCreate(data);
+        return res.status(200).json(status)
     } catch (error) {
         console.error(`Error al crear la sesión de entrenamiento \n ${error}`);
         return res.status(500).json('Internal server error');
     }
 }
 
-export const getById = async (req, res) =>{
+export const getById = async (req, res) => {
 
     const { idSuscripcion } = req.query;
 
-    console.log(`idSuscripcion: ${idSuscripcion}`);
-
-    if( !idSuscripcion ){return res.status(400).json('Missing fields')};
+    if (!idSuscripcion) { return res.status(400).json('Missing fields') };
 
     try {
         let sesiones = await SesionEntrenamiento.findAll({ where: { idSuscripcion } });
-        return res.status(200).json(sesiones)        
+        return res.status(200).json(sesiones)
     } catch (error) {
         console.log(`Error al recuperar sesiones de entrenamiento \n ${error}`);
         return res.status(500).json('Internal server error');
