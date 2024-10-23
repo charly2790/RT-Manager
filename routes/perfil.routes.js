44054212170c6f73e44b5278a8730cda47583d57
charly <cbarrionuevo@fidelatam.com>
2024-10-23 09:29:40 -0300

    Se crea route Perfil Update

--- routes/perfil.routes.js
+++ routes/perfil.routes.js
@@ -8,5 +8,6 @@ import { verifyToken } from "../middlewares/verifyToken.middleware.js";
 const router = Router();
 
 router.post('/perfiles', verifyToken, verifyPermisos('PROFILE_CREATE'), upload.single('profileImage'), validationRules, create);
+router.patch('/perfiles/:id', verifyToken, verifyPermisos('PROFILE_CREATE'), upload.single('profileImage'), update);
 
 export default router; 
