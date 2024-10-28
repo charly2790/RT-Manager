eeabd79506e7980f18e3344721c529200b706d7f
charly <cbarrionuevo@fidelatam.com>
2024-10-28 10:26:53 -0300

    creacion de nuevo endpoint para testing de subida de imagenes & ajustes en controller create profile

--- controllers/perfil.controller.js
+++ controllers/perfil.controller.js
@@ -5,7 +5,34 @@ import Perfil from '../models/Perfil.js';
 
 const redesSociales = ['Facebook', 'Instagram', 'X']
 
+export const testUpload = (req, res) => {
+    
+    let url = "http://prueba.com.ar"
+    
+    if (req.file) {
+        url = cloudinary.url(req.file.filename,
+            {
+                transformation: [{
+                    quality: 'auto',
+                    fetch_format: 'auto',
+                }, {
+                    width: 400,
+                    height: 400,
+                    crop: 'fill',
+                    gravity: 'auto',
+
+                }]
+            });
+    }
+
+    return res.status(200).json({ url });3
+
+
+}
+
 export const create = async (req, res) => {
+    console.log(req.body); // Verifica los campos del formulario
+    console.log(req.file); // Verifica el archivo cargado
 
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
@@ -48,8 +75,8 @@ export const create = async (req, res) => {
     }
 
     try {
-        const result = await Perfil.create(newProfile);
-        return res.status(200).json(result);
+        const perfil = await Perfil.create(newProfile);
+        return res.status(200).json({ message: 'Perfil creado correctamente', perfil });
     } catch (error) {
         console.error(`Error al crear el perfil \n${error.message}`);
         return res.status(500).json({ message: "Internal server error" });
@@ -72,7 +99,29 @@ export const update = async (req, res) => {
             updatedFields.redesSociales[red] = updatedFields[red];
             delete updatedFields[red]
         }
-    })    
+    });
+
+    
+    if (req.file) {                        
+        const url = cloudinary.url(req.file.filename,
+            {
+                transformation: [{
+                    quality: 'auto',
+                    fetch_format: 'auto',
+                }, {
+                    width: 400,
+                    height: 400,
+                    crop: 'fill',
+                    gravity: 'auto',
+
+                }]
+            });
+
+        console.log('url generada--->', url);
+        updatedFields.avatar = url;
+    }
+
+    console.log('updatedFields: ', updatedFields);
     
     try {
         perfil.update({ ...updatedFields });
--- routes/perfil.routes.js
+++ routes/perfil.routes.js
@@ -1,4 +1,4 @@
-import { create, update} from "../controllers/perfil.controller.js"
+import { create, update, testUpload } from "../controllers/perfil.controller.js"
 import { Router } from "express";
 import { upload } from '../middlewares/fileUpload.middleware.js'
 import { validationRules } from "../rules/perfil.rules.js";
@@ -9,5 +9,6 @@ const router = Router();
 
 router.post('/perfiles', verifyToken, verifyPermisos('PROFILE_CREATE'), upload.single('profileImage'), validationRules, create);
 router.patch('/perfiles/:id', verifyToken, verifyPermisos('PROFILE_CREATE'), upload.single('profileImage'), update);
+router.post('/testUpload', upload.single('profileImage'), testUpload);
 
 export default router; 
