5a9fff5dba2ec17e0093b91b474a2ea977f59b6e
charly <cbarrionuevo@fidelatam.com>
2024-12-13 18:47:39 -0300

    Se realizan ajustes en controllers de entrenamiento

--- controllers/entrenamientos.controller.js
+++ controllers/entrenamientos.controller.js
@@ -1,40 +1,128 @@
+import _ from "lodash";
+import { ErrorFactory } from "../utils/ErrorFactory.js";
+import { errorTypes } from "../utils/ErrorTypes.js";
 import Entrenamiento from "../models/Entrenamiento.js"
+import { errorMessages } from "../utils/ErrorMessages.js";
 
-export const create = async (req, res) => {    
-    
-    const { 
-        comentario, 
-        distancia,
-        idSesion, 
-        idUserLogged,
-        link, 
-        rpe, 
-        tiempoNeto, 
-        tiempoTotal, 
-    } = req.body;        
+export const create = async (req, res) => {
 
-    if( !idSesion || !distancia || !tiempoTotalSesion || !tiempoNetoSesion || !rpe){
-        return res.status(400).json({ message: 'Missing fields'});
-    }    
+    try {
+        const {
+            comentario,
+            distancia,
+            idSesion,
+            idUserLogged,
+            link,
+            rpe,
+            tiempoNeto,
+            tiempoTotal,
+        } = req.body;      
+        
+        if(!req.rol){
+            throw ErrorFactory.createError(errorTypes.VALIDATION_ERROR, errorMessages.ROLE_NOT_PROVIDED_ERROR)
+        }
 
-    const data = {
-        comentario,
-        distancia,
-        idSesion,
-        link:link?link:undefined,
-        rpe,
-        tiempoNeto: new Date(tiempoNeto).toTimeString().split(' ')[0],
-        tiempoTotal: new Date(tiempoTotal).toTimeString().split(' ')[0],
-        usuarioCreador:idUserLogged,
-        usuarioModificador:idUserLogged,
-    }    
+        let data = {};
 
-    try{
-        const newEntrenamiento = await Entrenamiento.create(data);
+        if(req.rol === 'EQUIPO_MEMBER'){
+            if(!idSesion || !link){
+                throw ErrorFactory.createError(errorTypes.VALIDATION_ERROR, errorMessages.MISSING_FIELDS)    
+            }
+
+            data = {
+                comentario,                
+                idSesion,
+                link,                                
+            }
+
+        }else if(req.rol === 'EQUIPO_ADMIN'){
+            if (!idSesion || !distancia || !tiempoTotalSesion || !tiempoNetoSesion || !rpe) {
+                throw ErrorFactory.createError(errorTypes.VALIDATION_ERROR, errorMessages.MISSING_FIELDS)
+            }
+            
+            data = {
+                comentario,
+                distancia,
+                idSesion,
+                link: link ? link : undefined,
+                rpe,
+                tiempoNeto: new Date(tiempoNeto).toTimeString().split(' ')[0],
+                tiempoTotal: new Date(tiempoTotal).toTimeString().split(' ')[0],
+                usuarioCreador: idUserLogged,
+                usuarioModificador: idUserLogged,
+            }
+        }        
+        let newEntrenamiento = await Entrenamiento.create(data);
+
+        if (!newEntrenamiento) {
+            throw ErrorFactory.createError(errorTypes.DATABASE_ERROR, errorMessages.INSERT_ERROR);
+        }
+
         return res.status(200).json(newEntrenamiento);
-    }catch(error){
-        console.error(`Error al crear entrenamiento \n${error}`);
-        return res.status(500).json({message:'internal server error'})
+    } catch (error) {
+        
+        let STATUS_CODE = 500;
+        let MESSAGE = 'Error interno del servidor';
+
+        if(error.name){
+            STATUS_CODE = error.statusCode;
+            MESSAGE = error.message;
+        }
+        console.log(STATUS_CODE, MESSAGE);
+
+        return res.status(STATUS_CODE).json({ message: MESSAGE });
+    }
+}
+
+export const patch = async (req, res) => {
+
+    try {
+        const { idEntrenamiento } = req.params;
+        const { idSesion, idUsuario } = req.body;
+
+        if (!idEntrenamiento || !idSesion) {
+            throw ErrorFactory.createError(errorTypes.VALIDATION_ERROR, 'Parámetros faltantes o no validos');
+        }
+
+        const updatedFields = { ...req.body };
+
+        delete updatedFields.idSesion;
+        delete updatedFields.idUsuario;
+
+        console.log('idEntrenamiento ---> ', idEntrenamiento);
+        console.log('idSesion ---> ', idSesion);
+
+        const entrenamiento = await Entrenamiento.findOne({
+            where: {
+                idEntrenamiento,
+                idSesion
+            }
+        });
+
+        console.log('entrenamiento ---> ', entrenamiento);
+
+        if (_.isEmpty(entrenamiento)) throw ErrorFactory.createError(errorTypes.VALIDATION_ERROR, `No se ha encontrado un entrenamiento asociado al id ${idEntrenamiento}`);
+
+        await entrenamiento.update({ ...updatedFields });
+
+        return res.status(200).json({
+            message: 'Entrenamiento Actualizado correctamente',
+            entrenamiento
+        })
+
+    } catch (error) {
+
+        let STATUS_CODE = 500;
+        let MESSAGE = 'Error interno del servidor';
+
+        switch (error.name) {
+            case errorTypes.VALIDATION_ERROR:
+                STATUS_CODE = error.statusCode;
+                MESSAGE = error.message;
+                break;
+        }
+
+        return res.status(STATUS_CODE).json({ message: MESSAGE });
     }
 
 }
--- routes/entrenamientos.routes.js
+++ routes/entrenamientos.routes.js
@@ -1,4 +1,4 @@
-import { create } from '../controllers/entrenamientos.controller.js';
+import { create, patch } from '../controllers/entrenamientos.controller.js';
 import { Router } from 'express';
 import { verifyToken } from '../middlewares/verifyToken.middleware.js';
 import { verifyPermisos } from '../middlewares/rbac.middleware.js';
@@ -7,6 +7,7 @@ import { upload } from '../middlewares/multipartFormData.middleware.js';
 const router = Router();
 
 router.post('/entrenamientos', verifyToken, verifyPermisos('ENTRENAMIENTO_READ'), upload.none(), create);
+router.patch('/entrenamientos/:idEntrenamiento', verifyToken, verifyPermisos('ENTRENAMIENTO_READ'), upload.none(), patch);
 
 export default router;
 
