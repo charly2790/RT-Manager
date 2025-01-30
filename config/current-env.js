6bf63f51224b18b6b262955da9498448c321d2f7
charly <cbarrionuevo@fidelatam.com>
2025-01-30 10:22:39 -0300

    Refactor current-env configuration to return an object and enhance validation rules by adding 'fechaEntrenamiento' field check

--- config/current-env.js
+++ config/current-env.js
@@ -1,9 +1,19 @@
 import { config } from "./config.js";
 
-export default  {
-    username: config.username,
-    password: config.password,
-    database: config.database,
-    host: config.host,    
-    dialect: config.dialect,
+export default ()=>{    
+    return {
+        host: config.host,    
+        dialect: config.dialect,
+        database: config.database,
+        username: config.username,
+        password: config.password,
+        port: config.port,
+        dialectOptions: config.ssl ? {
+            ssl: {
+                require: true,
+                rejectUnauthorized: false,
+                ca:config.ca,
+            },
+        }:{}
+    }
 };
